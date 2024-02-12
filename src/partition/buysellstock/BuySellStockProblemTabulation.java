122a6a0343202dc636c4dd073ce66eea3cad6816
PrabalSrivastava <prabalsrivastava99@gmail.com>
2024-02-13 02:07:15 +0530

    BuySellStockProblemTabulation -> TODO: Fix -> Fixed

--- src/partition/buysellstock/BuySellStockProblemTabulation.java
+++ src/partition/buysellstock/BuySellStockProblemTabulation.java
@@ -2,9 +2,7 @@ package partition.buysellstock;
 
 import util.DPUtil;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class BuySellStockProblemTabulation implements BuySellStockProblem{
 
@@ -20,28 +18,25 @@ public class BuySellStockProblemTabulation implements BuySellStockProblem{
         this.days = prices.size();
     }
 
-    //TODO: Fix for 1st test case
     @Override
     public Integer maxProfit() {
 
         @SuppressWarnings({"unchecked"})
         List<List<Integer>> dp = (List<List<Integer>>) DPUtil.init(Integer.class, this.days + 1, 2 * this.transactions + 1);
 
-        for (int i = 1; i <= this.days; i++) {
-            for (int j = 2 * this.transactions; j > 0; j--) {
+        for (int i = this.days - 1; i >= 0; i--) {
+            for (int j = 0; j < 2 * this.transactions; j++) {
                 assert dp != null;
-                int maxProfit = dp.get(i-1).get(j);
-                if(j % 2 != 0) maxProfit = Math.max(maxProfit, dp.get(i-1).get(j-1) + this.prices.get(i-1));
-                if(j % 2 == 0) maxProfit = Math.max(maxProfit, dp.get(i-1).get(j-1) - this.prices.get(i-1));
+                int maxProfit = dp.get(i+1).get(j);
+                if(j % 2 != 0) maxProfit = Math.max(maxProfit, dp.get(i+1).get(j+1) + this.prices.get(i));
+                if(j % 2 == 0) maxProfit = Math.max(maxProfit, dp.get(i+1).get(j+1) - this.prices.get(i));
 
                 dp.get(i).set(j, maxProfit);
             }
         }
 
         assert dp != null;
-        return dp.get(this.days).get(2 * this.transactions);
+        return dp.get(0).get(0);
     }
-
-
 }
 
